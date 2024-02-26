e3e40fdec72e3e09c3e6797c34b75525903debe7
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-02-26 16:46:23 +0500

    Write Business Logic

---
+++ Classes/Meva.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fruit.Classes
+{
+    public class Meva
+    {
+        public string name;
+        public string color;
+
+        //property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Fruit.Classes;
+//Call Meva for apple
+Meva apple = new Meva();
+apple.name = "Golden";
+apple.color = "Red";
+Console.WriteLine($"Apple is name: {apple.name}\nApple is color: {apple.Color}");
+
+//Call Meva for banana
+Meva banana = new Meva();
+banana.name = "Banana";
+banana.color = "Yellow";
+Console.WriteLine($"Banana is name: {banana.name}\nBanana is color: {banana.Color}");
+
+//Call Meva for pomegranate
+Meva pomegranate = new Meva();
+pomegranate.name = "Pomegranate";
+pomegranate.color = "Red";
+Console.WriteLine($"Pomegranate is name: {pomegranate.name}\nPomegranate is color: {pomegranate.Color}");
+
